f9e675178fe03c0f1cc5f377aa75ad6db10a8291
bogo-phha <PHHA@bang-olufsen.dk>
2022-11-03 12:54:25 +0100

    dualFF plot v1.0
    Tool to plot characterization data with multiple FF mics

--- Measurements.py
+++ Measurements.py
@@ -19,53 +19,124 @@ class Measurements:
 class Measurement:
     path = None
     def __init__(self):
-        self.keys = ["FR",
-                "THD",
-                "AOP",
-                ]
+        self.keys = ["A2E",
+                "A2FF1",
+                "A2FF2",
+                "A2FF3",
+                "A2FF4",
+                "A2FF5",
+                "A2FF6",
+                "A2FF7",
+                "A2FF8",
+                "A2FF9",
+                "A2FF10",
+                "A2FB",
+                "D2E",
+                "D2FB",
+                "A2E(open)",
+                "Passive",
+                "FF1TargetEar",
+                "FF2TargetEar",
+                "FF3TargetEar",
+                "FF4TargetEar",
+                "FF5TargetEar",
+                "FF6TargetEar",
+                "FF7TargetEar",
+                "FF8TargetEar",
+                "FF9TargetEar",
+                "FF10TargetEar",
+                "FF1TargetFB",
+                "FF2TargetFB",
+                "FF3TargetFB",
+                "FF4TargetFB",
+                "FF5TargetFB",
+                "FF6TargetFB",
+                "FF7TargetFB",
+                "FF8TargetFB",
+                "FF9TargetFB",
+                "FF10TargetFB",
+                "D2E/D2FB",
+                "A2E/A2FB",
+                "(D2E/D2FB)/(A2E/A2FB)",
+                "1/D2E",
+                "1/D2FB"]
         self.H = dict()
+        self.f = np.logspace(0,4,500)
 
         # Further Parameters
         self.label = ""
 
-        self.ampoffset = 0
-        self.ampnormalize = False
-        self.ampnormalizefreq= 0
-        self.phaoffset = 0
-        self.phanormalize = False
-        self.phanormalizefreq= 0
+        self.driverCalibration = 0
+        self.invertDriver = False
+
+        self.driverPhase = 0
+        self.FFTargetEarPhase = 0
+        self.FFTargetFBPhase = 0
+        self.export = False
 
         # Parse the available data
         self.parse_file()
 
+        # Fill the voids
+        for key in self.keys:
+            try:
+                a = self.H["o"+key]
+            except KeyError:
+                self.H["o"+key] = np.ones(np.shape(self.f))
+
         # Calculate the derived transfer functions
-        self.calculateTransferFunctions()
+        self.caluclateTransferFunctions()
 
-    def calculateTransferFunctions(self):
+    def caluclateTransferFunctions(self):
+        self.H["D2E"] = self.H["oD2E"] * 10**(self.driverCalibration/20) * np.e**( 1j * np.deg2rad(180*self.invertDriver) )
+        self.H["D2FB"] = self.H["oD2FB"] * 10**(self.driverCalibration/20)* np.e**( 1j * np.deg2rad(180*self.invertDriver) )       
+        self.H["A2E"] = self.H["oA2E"]
+        self.H["A2E(open)"] = self.H["oA2E(open)"]
+        self.H["A2FB"] = self.H["oA2FB"]
+        self.H["A2FF1"] = self.H["oA2FF1"]
+        self.H["A2FF2"] = self.H["oA2FF2"]
+        self.H["A2FF3"] = self.H["oA2FF3"]
+        self.H["A2FF4"] = self.H["oA2FF4"]
+        self.H["A2FF5"] = self.H["oA2FF5"]
+        self.H["A2FF6"] = self.H["oA2FF6"]
+        self.H["A2FF7"] = self.H["oA2FF7"]
+        self.H["A2FF8"] = self.H["oA2FF8"]
+        self.H["A2FF9"] = self.H["oA2FF9"]
+        self.H["A2FF10"] = self.H["oA2FF10"]
         
-        # if self.H["oMagRefx"] != None:
-        #     pass
-        #     # self.H["MagY"] = self.H["oMagy"] - self.H["oMagRefy"]
-        #     # self.H["PhaY"] = self.H["oPhay"] - self.H["oPhaRefy"]
-        # else:
         
-        if self.ampnormalize == False:
-            self.H["MagY"] = self.H["oMagY"] + self.ampoffset
-        else: 
-            [idx, value] = find_nearest(self.H["oMagX"], self.ampnormalizefreq)  
-            self.H["MagY"] = self.H["oMagY"] - self.H["oMagY"][idx]        
+        
+        self.H["FF1TargetEar"] = self.H["A2E"] / (self.H["A2FF1"] * self.H["D2E"])
+        self.H["FF1TargetFB"] = self.H["A2FB"] / (self.H["A2FF1"] * self.H["D2FB"])
+        self.H["FF2TargetEar"] = self.H["A2E"] / (self.H["A2FF2"] * self.H["D2E"])
+        self.H["FF2TargetFB"] = self.H["A2FB"] / (self.H["A2FF2"] * self.H["D2FB"])
+        self.H["FF3TargetEar"] = self.H["A2E"] / (self.H["A2FF3"] * self.H["D2E"])
+        self.H["FF3TargetFB"] = self.H["A2FB"] / (self.H["A2FF3"] * self.H["D2FB"])
+        self.H["FF4TargetEar"] = self.H["A2E"] / (self.H["A2FF4"] * self.H["D2E"])
+        self.H["FF4TargetFB"] = self.H["A2FB"] / (self.H["A2FF4"] * self.H["D2FB"])
+        self.H["FF5TargetEar"] = self.H["A2E"] / (self.H["A2FF5"] * self.H["D2E"])
+        self.H["FF5TargetFB"] = self.H["A2FB"] / (self.H["A2FF5"] * self.H["D2FB"])
+        self.H["FF6TargetEar"] = self.H["A2E"] / (self.H["A2FF6"] * self.H["D2E"])
+        self.H["FF6TargetFB"] = self.H["A2FB"] / (self.H["A2FF6"] * self.H["D2FB"])
+        self.H["FF7TargetEar"] = self.H["A2E"] / (self.H["A2FF7"] * self.H["D2E"])
+        self.H["FF7TargetFB"] = self.H["A2FB"] / (self.H["A2FF7"] * self.H["D2FB"])
+        self.H["FF8TargetEar"] = self.H["A2E"] / (self.H["A2FF8"] * self.H["D2E"])
+        self.H["FF8TargetFB"] = self.H["A2FB"] / (self.H["A2FF8"] * self.H["D2FB"])
+        self.H["FF9TargetEar"] = self.H["A2E"] / (self.H["A2FF9"] * self.H["D2E"])
+        self.H["FF9TargetFB"] = self.H["A2FB"] / (self.H["A2FF9"] * self.H["D2FB"])
+        self.H["FF10TargetEar"] = self.H["A2E"] / (self.H["A2FF10"] * self.H["D2E"])
+        self.H["FF10TargetFB"] = self.H["A2FB"] / (self.H["A2FF10"] * self.H["D2FB"])
+        
+        
+        self.H["Passive"] = self.H["A2E"] / self.H["A2E(open)"]
+        self.H["D2E/D2FB"] = self.H["D2E"] / self.H["D2FB"]
 
-        self.H["MagX"] = self.H["oMagX"] 
-        self.H["PhaX"] = self.H["oPhaX"]
-        self.H["PhaY"] = self.H["oPhaY"]
-        self.H["AopX"] = self.H["oAopX"]
-        self.H["AopY"] = self.H["oAopY"]
-        self.H["ThdX"] = self.H["oThdX"]
-        self.H["ThdY"] = self.H["oThdY"]
-        self.H["Snr"] = self.H["oSnr"] 
-        self.H["Thd"] = self.H["oThd"] 
-        self.H["Sens"] = self.H["oSens"]
-        
+        self.H["A2E/A2FB"] = self.H["A2E"] / self.H["A2FB"]
+        self.H["(D2E/D2FB)/(A2E/A2FB)"] = (self.H["D2E"] / self.H["D2FB"]) / (self.H["A2E"] / self.H["A2FB"])
+
+        self.H["1/D2E"] = 1/self.H["D2E"]
+        self.H["1/D2FB"] = 1/self.H["D2FB"]
+
 
     def setDriverCalibration(self, offset):
         self.driverCalibration = offset
@@ -78,35 +149,105 @@ class Measurement:
     def parse_file(self):
         pass
 
-class Mic(Measurement):
+
+class FreD(Measurement):
     def __init__(self, path):
         self.path = path
         super().__init__()
-        
+
     def parse_file(self):
-        file = "Mic Measurement"
-        data = None
-        if os.path.exists(self.path + "/" + file + ".xlsx"):
-            data = ApXls.MeasurementMic(self.path + "/" + file + ".xlsx")
-        if os.path.exists(self.path + "/" + file + ".xls"):
-            data = ApXls.MeasurementMic(self.path + "/" + file + ".xls")
+        data = ApXls.MeasurementSet(self.path)
+        self.f = data.x
+        self.H["oD2E"] = toComplex(data.Y[10], data.Y[11])
+        self.H["oD2FB"] = toComplex(data.Y[6], data.Y[7])
+        self.H["oD2FF"] = toComplex(data.Y[2], data.Y[3])
+        self.H["oA2E"] = toComplex(data.Y[8], data.Y[9])
+        self.H["oA2FB"] = toComplex(data.Y[4], data.Y[5])
+        self.H["oA2FF"] = toComplex(data.Y[0], data.Y[1])
 
-        self.H["oMagX"] = data.magx
-        self.H["oMagY"] = data.magy
-        self.H["oPhaX"] = data.phax
-        self.H["oPhaY"] = data.phay
-        self.H["oAopX"] = data.aopx
-        self.H["oAopY"] = data.aopy
-        self.H["oThdX"] = data.thdx
-        self.H["oThdY"] = data.thdy
-        self.H["oSnr"] = data.snr
-        self.H["oThd"] = data.thd
-        self.H["oSens"] = data.sens
+class FleX(Measurement):
+    def __init__(self, path):
+        self.path = path
+        super().__init__()
 
+    def parse_file(self):
+        # Parse folder
+        files = ["D2E", "D2FB", "D2FF", "A2E", "A2FB", "A2FF", "A2E(open)"]
+        self.f = ApXls.MeasurementSet(self.path + "/" + "D2FB.xlsx").x
+
+        for file in files:
+            if os.path.exists(self.path + "/" + file + ".xlsx"):
+                data = ApXls.MeasurementSet(self.path + "/" + file + ".xlsx")
+                mag = data.Y[0]
+                phs = data.Y[1]
+                h = toComplex(mag, phs)
+                self.H["o"+file] = h
+            else:
+                # Fill in zeros
+                self.H[file] = 1j* 1e-200 * np.ones( np.shape(self.f) )
         # Define the additional parameters
-        self.label = self.path[-60:]
+        self.label = self.path[-80:]
+        
+class DualFF(Measurement):
+    def __init__(self, path):
+        self.path = path
+        super().__init__()
 
-def find_nearest(array, value):
-    array = np.asarray(array)
-    idx = (np.abs(array - value)).argmin()
-    return [idx, array[idx]]
+    def parse_file(self):
+        # Parse folder
+        files = ["D2E", "D2FB", "A2E", "A2FB", "A2FF1", "A2FF2", "A2FF3", "A2FF4", "A2FF5", "A2FF6", "A2FF7", "A2FF8", "A2FF9", "A2FF10", "A2E(open)"]
+        self.f = ApXls.MeasurementSet(self.path + "/" + "D2FB.xlsx").x
+
+        for file in files:
+            if os.path.exists(self.path + "/" + file + ".xlsx"):
+                data = ApXls.MeasurementSet(self.path + "/" + file + ".xlsx")
+                mag = data.Y[0]
+                phs = data.Y[1]
+                h = toComplex(mag, phs)
+                self.H["o"+file] = h
+            else:
+                # Fill in zeros
+                self.H[file] = 1j* 1e-200 * np.ones( np.shape(self.f) )
+        # Define the additional parameters
+        self.label = self.path[-80:]
+
+"""
+class FFAnalog(Measurement):
+    def __init__(self, path):
+        self.path = path
+        super().__init__()
+
+    def parse_file(self):
+        data = ApXls.MeasurementSet(self.path)
+        self.f = data.x
+        self.H["A2E"] = toComplex( data.Y[0], data.Y[2] )
+        self.H["A2FF"] = toComplex( data.Y[1], data.Y[3] )
+        self.H["D2E"] = toComplex( data.Y[4], data.Y[6] )
+        self.H["D2FF"] = toComplex( data.Y[5], data.Y[7] )
+
+class FBAnalog(Measurement):
+    def __init__(self, path):
+        self.path = path
+        super().__init__()
+
+    def parse_file(self):
+        data = ApXls.MeasurementSet(self.path)
+        self.f = data.x
+        self.H["D2E"] = toComplex( data.Y[0], data.Y[2] )
+        self.H["D2FB"] = toComplex( data.Y[1], data.Y[3] )
+
+This was some other template:
+class AnotherTemplate(Measurement):
+    def __init__(self, path):
+        self.path = path
+        super().__init__()
+
+    def parse_file(self):
+        data = ApXls.MeasurementSet(self.path)
+        self.f = data.x
+        self.H["D2E"] = toComplex(data.Y[0], data.Y[1])
+        self.H["A2E"] = toComplex(data.Y[2], data.Y[3])
+        self.H["D2FB"] = toComplex(data.Y[4], data.Y[5])
+        self.H["A2FB"] = toComplex(data.Y[6], data.Y[7])
+        self.H["A2FF"] = toComplex(data.Y[8], data.Y[9])
+"""
--- ParameterTable.py
+++ ParameterTable.py
@@ -6,12 +6,12 @@ class ParameterTable():
         self.table = ipysheet.sheet(rows=len(measurements),
                                columns=7,
                                column_headers=["Label",
-                                               "Amp Offset",
-                                               "Amp Normalize",
-                                               "Amp Normalize Freq",
-                                               "Phase Offset",
-                                               "Phase Normalize",
-                                               "Phase Normalize Freq"],
+                                               "Driver Calibration",
+                                               "Invert Driver",
+                                               "Driver Phase",
+                                               "FF Target Ear Phase",
+                                               "FF Target FB Phase",
+                                               "Export"],
                                row_headers=False)
         self.rows = []
         self.cells = []
@@ -23,35 +23,35 @@ class ParameterTable():
             columns["Label"] = cell_label
             self.cells.append(cell_label)
 
-            cell_ampoffset = ipysheet.cell(i, 1, self.measurements[i].ampoffset)
-            cell_ampoffset.observe(self.onChange, 'value')
-            columns["Amp Offset"] = cell_ampoffset
-            self.cells.append(cell_ampoffset)
+            cell_driver = ipysheet.cell(i, 1, self.measurements[i].driverCalibration)
+            cell_driver.observe(self.onChange, 'value')
+            columns["Driver Calibration"] = cell_driver
+            self.cells.append(cell_driver)
 
-            cell_ampnormalize = ipysheet.cell(i, 2, self.measurements[i].ampnormalize)
-            cell_ampnormalize.observe(self.onChange, 'value')
-            columns["Amp Norm"] = cell_ampnormalize
-            self.cells.append(cell_ampnormalize) 
+            cell_driver_invert = ipysheet.cell(i, 2, self.measurements[i].invertDriver)
+            cell_driver_invert.observe(self.onChange, 'value')
+            columns["Invert Driver"] = cell_driver_invert
+            self.cells.append(cell_driver_invert)
 
-            cell_ampnormalizefreq = ipysheet.cell(i, 3, self.measurements[i].ampnormalizefreq)
-            cell_ampnormalizefreq.observe(self.onChange, 'value')
-            columns["Amp Norm Freq"] = cell_ampnormalizefreq
-            self.cells.append(cell_ampnormalizefreq)
+            cell_driver_phase = ipysheet.cell(i, 3, self.measurements[i].driverPhase)
+            cell_driver_phase.observe(self.onChange, 'value')
+            columns["Driver Phase"] = cell_driver_phase
+            self.cells.append(cell_driver_phase)
 
-            cell_phaoffset = ipysheet.cell(i, 4, self.measurements[i].phaoffset)
-            cell_phaoffset.observe(self.onChange, 'value')
-            columns["Phase Offset"] = cell_phaoffset
-            self.cells.append(cell_phaoffset)
+            cell_FFTargetEarPhase = ipysheet.cell(i, 4, self.measurements[i].FFTargetEarPhase)
+            cell_FFTargetEarPhase.observe(self.onChange, 'value')
+            columns["FF Target Ear Phase"] = cell_FFTargetEarPhase
+            self.cells.append(cell_FFTargetEarPhase)
 
-            cell_phanormalize = ipysheet.cell(i, 5, self.measurements[i].phanormalize)
-            cell_phanormalize.observe(self.onChange, 'value')
-            columns["Phase Norm"] = cell_phanormalize
-            self.cells.append(cell_phanormalize)
+            cell_FFTargetFBPhase = ipysheet.cell(i, 5, self.measurements[i].FFTargetFBPhase)
+            cell_FFTargetFBPhase.observe(self.onChange, 'value')
+            columns["FF Target FB Phase"] = cell_FFTargetFBPhase
+            self.cells.append(cell_FFTargetFBPhase)
 
-            cell_phanormalizefreq = ipysheet.cell(i, 6, self.measurements[i].phanormalizefreq)
-            cell_phanormalizefreq.observe(self.onChange, 'value')
-            columns["Phase Norm Freq"] = cell_phanormalizefreq
-            self.cells.append(cell_phanormalizefreq)
+            cell_export = ipysheet.cell(i, 6, self.measurements[i].export)
+            cell_export.observe(self.onChange, 'value')
+            columns["Export"] = cell_export
+            self.cells.append(cell_export)
 
             self.rows.append(columns)
 
@@ -59,31 +59,11 @@ class ParameterTable():
         for i in range(len(self.measurements)):
             self.measurements[i].label = self.rows[i]["Label"].value
 
-            self.measurements[i].ampoffset = self.rows[i]["Amp Offset"].value 
-            self.measurements[i].ampnormalize = self.rows[i]["Amp Norm"].value
-            self.measurements[i].ampnormalizefreq = self.rows[i]["Amp Norm Freq"].value
-            self.measurements[i].phaoffset = self.rows[i]["Phase Offset"].value
-            self.measurements[i].phanormalize     = self.rows[i]["Phase Norm"].value
-            self.measurements[i].phanormalizefreq = self.rows[i]["Phase Norm Freq"].value
-            self.measurements[i].calculateTransferFunctions()
-            
-class ParameterTable_Meas():
-    def __init__(self, measurements):
-        self.measurements = measurements
-        self.table = ipysheet.sheet(rows=len(measurements),
-                               columns=4,
-                               column_headers=["-Mic-",
-                                               "SNR",
-                                               "Sensitivity",
-                                               "THD"
-                                               ],
-                               row_headers=False)
+            self.measurements[i].setDriverCalibration( self.rows[i]["Driver Calibration"].value )
+            self.measurements[i].setDriverInversion( self.rows[i]["Invert Driver"].value)
 
-        for i in range(len(self.measurements)):
+            self.measurements[i].driverPhase = self.rows[i]["Driver Phase"].value
+            self.measurements[i].FFTargetEarPhase = self.rows[i]["FF Target Ear Phase"].value
+            self.measurements[i].FFTargetFBPhase = self.rows[i]["FF Target FB Phase"].value
 
-            cell_Label = ipysheet.cell(i, 0, self.measurements[i].label[-60:])
-            cell_Snr = ipysheet.cell(i, 1, self.measurements[i].H["oSnr"])
-            cell_Sens = ipysheet.cell(i, 2, self.measurements[i].H["oSens"])
-            cell_Thd = ipysheet.cell(i, 3, self.measurements[i].H["oThd"])
-
-    
+            self.measurements[i].export = self.rows[i]["Export"].value
